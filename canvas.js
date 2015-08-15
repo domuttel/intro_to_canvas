7529f9db30e29820d54f67ee9cc6b0b54707658b
Dominic Muttel <dominic.muttel@gmail.com>
2015-08-14 21:52:57 -0600

    Initial commit -Iteration Zero

--- canvas.js
+++ canvas.js
@@ -1,6 +1,16 @@
-// function drawBorder(){
-//     var canvas = document.getElementById("canvas");
-//     var context = canvas.getContext('2d');
-//     var size = { x: canvas.width, y: canvas.height }
-//     context.strokeRect(0,0, size.x, size.y);
+function drawBorder(){
+    var canvas = document.getElementById("canvas");
+    console.log(canvas);
+    var context = canvas.getContext('2d');
+    var size = { x: canvas.width, y: canvas.height };
+    context.strokeRect(0,0, size.x, size.y);
+}
+drawBorder();
+
+//Iteration One: Drawing Rectangles
+
+// function drawRectangle(){
+//   var fillStyle = ('green');
+//   context.fillRect(0,0, size.x, size.y);
 // }
+// drawRectangle();
